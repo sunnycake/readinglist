da2d321eee3a75a7878e26ed97f6ab268a1dd9f6
whagestad <Slinky!12>
2020-09-18 13:23:44 -0500

    Adds delete option (#7) to the menu. User must type DELETE to delete a book, otherwise it returns to the main menu. Note: Attempting to delete a book not in the database will cause an error- this will be addressed in a separate branch/pull request

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ def create_menu():
     menu.add_option('4', 'Show Read Books', show_read_books)
     menu.add_option('5', 'Show All Books', show_all_books)
     menu.add_option('6', 'Change Book Read Status', change_read)
+    menu.add_option('7', 'Delete Book', delete_book)
     menu.add_option('Q', 'Quit', quit_program)
 
     return menu
@@ -64,6 +65,17 @@ def change_read():
     new_read = ui.get_read_value()     
     book.read = new_read 
     book.save()
+
+def delete_book():
+
+    book_id = ui.get_book_id()
+    book = store.get_book_by_id(book_id)
+    answer = ui.ask_question("Type DELETE to confirm you'd like to delete this book: ")
+    if answer == "DELETE":
+        Book.delete(book)
+    else:
+        ui.message("Returning to main menu. Book has not been deleted.")
+        main()
     
 
 def quit_program():
