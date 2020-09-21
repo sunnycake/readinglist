56fe47f4c3b1a3c3aa9eb960193359349c7338c4
sunnycake <mynuex@gmail.com>
2020-09-20 20:38:49 -0500

    Add try/except for inserting existing book

--- main.py
+++ main.py
@@ -35,7 +35,10 @@ def create_menu():
 
 def add_book():
     new_book = ui.get_book_info()
-    new_book.save()
+    try:
+        new_book.save()
+    except:
+        ui.message('\nError - The book you\'re trying to add already exists.\n')
 
 
 def show_read_books():
